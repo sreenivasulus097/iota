8348d487234540695bf81a2ca42fbcb77962fde2
sreenivasulu.s097 <sreenivasulu.s097@gmail.com>
2022-06-10 16:54:17 +0530

    actual data updated for features component

--- src/data/featuresData.js
+++ src/data/featuresData.js
@@ -12,14 +12,14 @@ export const featuresData = [
         "category" : "socialVideo",
 "title" :"Identity",
 "image":"/images/fingerprint.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"description":"Bringing digital identity beyond borders to all people."
     },
     {
         "id":3,
         "category" : "socialVideo",
 "title" :"Citizen-Centric Governance",
-"image":"/images/green.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"image":"/images/Citizen.png",
+"description":"Enabling more transparent and inclusive public services."
     },
   
   
@@ -27,22 +27,22 @@ export const featuresData = [
         "id":4,
         "category" : "mobilityVideo",
 "title" :"Car Wallets",
-"image":"/images/p2p.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"image":"/images/Car_Wallet.svg",
+"description":"In-built wallets allow cars to send and receive payments."
     },
     {
         "id":5,
         "category" : "mobilityVideo",
 "title" :"Smart Charging",
-"image":"/images/fingerprint.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"image":"/images/charging.svg",
+"description":"Electric vehicles can autonomously pay for charging."
     },
     {
         "id":6,
         "category" : "mobilityVideo",
 "title" :"Usage Based Insurance",
-"image":"/images/green.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"image":"/images/Usage_Based_Insurance.svg",
+"description":"Insurance policies based on actual usage behaviours."
     } ,
   
     {
@@ -50,21 +50,21 @@ export const featuresData = [
         "category" : "smartcityVideo",
 "title" :"Urban Mobility Management",
 "image":"/images/urban.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"description":"Use transport and infrastructure data for informed decision making."
     },
     {
         "id":8,
         "category" : "smartcityVideo",
 "title" :"Open & Transport Data",
 "image":"/images/transact.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"description":"Centrally display, manage and analyze available data."
     },
     {
         "id":9,
         "category" : "smartcityVideo",
 "title" :"Peer to Peer Collaboration",
 "image":"/images/p2p.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"description":"Build resilient infrastructure designed for the sharing economy."
     } ,
   
     {
@@ -72,21 +72,21 @@ export const featuresData = [
         "category" : "globalTradeVideo",
 "title" :"Track & Trace",
 "image":"/images/track.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"description":"Share status throughout the supply chain with provenance data from the source."
     },
     {
         "id":11,
         "category" : "globalTradeVideo",
 "title" :"Customs & Border Management",
-"image":"/images/track.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"image":"/images/Customs___Border_Management.png",
+"description":"Upgrade trade certificates for international exchange."
     },
     {
         "id":12,
         "category" : "globalTradeVideo",
 "title" :"Digital Twins",
 "image":"/images/box.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"description":"Turn any product, pallet, container or consignment into a digital twin."
     },
    
   
@@ -95,21 +95,21 @@ export const featuresData = [
         "category" : "digitalVideo",
 "title" :"Self Soverign Identity",
 "image":"/images/research.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"description":"A borderless digital identity can be verified by anyone."
     },
     {
         "id":14,
         "category" : "digitalVideo",
 "title" :"Identity of Things",
 "image":"/images/remote.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"description":"Identities allow devices to prove their specifications and authenticity."
     },
     {
         "id":15,
         "category" : "digitalVideo",
 "title" :"Regulatory Compliance",
 "image":"/images/healthcare.png",
-"description":"Harnessing the power of IoT to manage natural resources more sustainbly."
+"description":"Digital identities that meet data privacy regulations like GDPR."
     }
   
 ]
