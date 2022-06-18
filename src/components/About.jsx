bc527660e99038c2cce7626e1c3248f219f5fa92
srishivams <you@example.com>
2022-06-18 11:13:26 +0530

    appending footer,About and Hero components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,16 @@
+import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react';
+import styles from '../styles/about.module.css';
 import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <Flex alignItems='center' justifyContent='center' direction={['column']} bgColor="white" h={['85vh', '110vh']}>
+      <Heading color='teal.300' fontSize='md' my={8}>INTRODUCING</Heading>
+      <Heading color='black' w={['90%', '32rem']}>An Open, Feeless Data And Value Transfer Protocol</Heading>
+      <Box className={styles.box} my={8}></Box>
+      <Text color='black' w={['90%', '30rem']}>IOTA has fundamentally reengineered distributed ledger technology, enabling secure exchange of both value and data, without any fees.</Text>
+      <Button _hover={{ bg: "teal.400" }} bg='teal.600' my={8} p={6}>LEARN ABOUT IOTA</Button>
+    </Flex>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div>
-      Nav
+    <div style={{overflow:'visible',opacity:"0.5",position:"fixed",top:"0",width:"100%",zIndex:'2'}}>
+        <h1 style={{color:"white"}}>Navbar</h1>
     </div>
   );
 }
